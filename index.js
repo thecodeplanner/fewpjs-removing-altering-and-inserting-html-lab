141ea67339973ac6340d576de80be641b040bc56
thecodeplanner <thecodeplanner@gmail.com>
2020-12-10 23:12:30 -0500

    commit tree 696eb97ef0237779e25ad21a86501af07bfa7ffa

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your code here!
+// let element = document.createElement('div')
+
+// document.body.appendChild(element) 
+
+// let ul = document.createElement('ul')
+
+// for (let i = 0; i < 3; i++) {
+//   let li = document.createElement('li')
+//   li.innerHTML = (i + 1).toString()
+//   ul.appendChild(li)
+// }
+
+// element.appendChild(ul) 
+
+
+// element.style.backgroundColor = '#27647B'; 
+
+// element.style.textAlign = 'center';
+// ul.style.textAlign = 'left' 
+
+document.querySelector('main#main').remove()
+
+let newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = "Your-Name is the champion"
